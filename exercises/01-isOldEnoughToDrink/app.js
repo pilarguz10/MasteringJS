0e6134f9f084284a10494e0b2d65ae07c92a6329
pilarguz10 <pilar1494@hotmail.com>
2020-12-08 18:24:04 +0000

    Hasta el 5

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,8 @@
-function isOldEnoughToDrink(){
+function isOldEnoughToDrink(age){
     // your code here	
+    if (age >= 21){
+        return true;
+    }else{
+        return false;;
+    }
 }
--- exercises/04-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/04-isOldEnoughToDrinkAndDrive/app.js
@@ -1,3 +1,8 @@
 function isOldEnoughToDrinkAndDrive(age) {
   // your code here
+   if (age >= 21){
+        return false;
+    }else{
+        return false;
+}
 }
