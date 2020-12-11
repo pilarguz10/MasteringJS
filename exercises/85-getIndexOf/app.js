7396a7c05ed400429e5f1d8661dbd0238710675f
pilarguz10 <pilar1494@hotmail.com>
2020-12-11 20:35:50 +0000

    Ej 86

--- exercises/85-getIndexOf/app.js
+++ exercises/85-getIndexOf/app.js
@@ -1,1 +1,25 @@
 // Write your function here
+function getIndexOf(caracter, palabra)
+{
+//   let arr = [];
+//   for (let index = 0; index < palabra.length; index++) 
+//   {
+      
+//      if(palabra[index] === caracter)
+//      {
+        
+//          arr.push(index);
+//      }
+    
+      
+//   }
+//    return arr;
+ 
+// 
+
+    return palabra.indexOf(caracter);
+}
+
+
+var output = getIndexOf('a', 'I am a hacker');
+console.log(output); // --> 2
--- exercises/86-findMinLengthOfThreeWords/app.js
+++ exercises/86-findMinLengthOfThreeWords/app.js
@@ -1,1 +1,14 @@
 // Write your function here
+function findMinLengthOfThreeWords(word1, word2, word3)
+{
+    let cant1 = word1.length;
+    let cant2 = word2.length;
+    let cant3 = word3.length;
+
+    let menor = Math.min(cant1, cant2, cant3);
+    return menor;
+    
+}
+
+var output = findMinLengthOfThreeWords('aa', 'beaaa', 'seeaaaa');
+console.log(output); // --> 1
